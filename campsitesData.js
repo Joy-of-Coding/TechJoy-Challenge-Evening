f6f51ca257a3145fa5438dba9bc27ce14f563989
Hope-Barnett <hopekarola2009@live.com>
2026-07-24 20:16:26 -0400

    Updated Arizona Desert Campground type from "forest" to "desert"  and  Updated New Mexico High Desert type from "mountain" to "desert"

--- campsitesData.js
+++ campsitesData.js
@@ -896,8 +896,8 @@ const campsitesData = [
             "Bird watching",
             "Sunset viewing"
         ],
-        "type": "forest",
-        "tags": ["forest", "desert", "hiking", "arizona"]
+        "type": "desert",
+        "tags": ["desert", "hiking", "arizona"]
     },
     {
         "id": 33,
@@ -924,8 +924,8 @@ const campsitesData = [
             "Stargazing",
             "Bat watching"
         ],
-        "type": "mountain",
-        "tags": ["mountain", "desert", "caves", "newmexico"]
+        "type": "desert",
+        "tags": ["desert", "caves", "newmexico"]
     },
     {
         "id": 34,
